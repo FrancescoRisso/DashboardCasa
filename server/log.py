8c582c4acd0398a2d81b9905e67b3b993ddd117f
FrancescoRisso <49359875+FrancescoRisso@users.noreply.github.com>
2022-08-11 19:30:10 +0000

    Version 1.2.5
    BUG FIXES:
    - Fixed some wrong settings for gh action

--- server/log.py
+++ server/log.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def printLog(app, type, request, message):
 	if type == "Err":
 		# Error log
-		app.logger.error((f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [ERR ] {message}")
+		app.logger.error(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [ERR ] {message}")
 	else:
 		# Info log
-		app.logger.info((f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [INFO] {request.remote_addr}: {message}")
+		app.logger.info(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [INFO] {request.remote_addr}: {message}")
