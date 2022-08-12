da88f8eda75e1d156d4bd5b10373f6f15bac21de
FrancescoRisso <49359875+FrancescoRisso@users.noreply.github.com>
2022-08-12 09:57:48 +0000

    Version 1.2.6
    BUG FIXES:
    - Server was not able to log
    - Corrected errors in the README-BUILD.md

--- server/log.py
+++ server/log.py
@@ -3,8 +3,6 @@ from datetime import datetime
 
 def printLog(app, type, request, message):
 	if type == "Err":
-		# Error log
-		app.logger.error(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [ERR ] {message}")
+		app.logger.error(f"{message}")
 	else:
-		# Info log
-		app.logger.info(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} [INFO] {request.remote_addr}: {message}")
+		app.logger.info(f"{request.remote_addr}: {message}")
