3323a569aeb64671ff545cab129770bccd62d28e
FrancescoRisso <49359875+FrancescoRisso@users.noreply.github.com>
2025-12-17 15:49:36 +0000

    Added possibility to change set temperatures
    Still only server-side

--- server/functions/constants.py
+++ server/functions/constants.py
@@ -3,12 +3,16 @@ class Room:
         self,
         name: str,
         heatingPage: int | None = None,
+        heatingCode: str | None = None,
         coolingPage: int | None = None,
+        coolingCode: str | None = None,
         alter: str | None = None,
     ) -> None:
         self.name = name
         self.__heatingPage = heatingPage
+        self.__heatingCode = heatingCode
         self.__coolingPage = coolingPage
+        self.__coolingCode = coolingCode
         self.__alter = alter
 
     def page(self, heating: bool) -> int:
@@ -16,17 +20,55 @@ class Room:
         assert page is not None
         return page
 
+    def code(self, heating: bool) -> str:
+        code = self.__heatingCode if heating else self.__coolingCode
+        assert code is not None
+        return code
+
     @property
     def disp_name(self) -> str:
         return self.__alter if self.__alter is not None else self.name
 
 
 main_rooms: list[Room] = [
-    Room("Salone", heatingPage=9, coolingPage=12),
-    Room("Studio", heatingPage=7, coolingPage=10),
-    Room("Camera_genitori", alter="Camera genitori", heatingPage=8, coolingPage=11),
-    Room("Camera_Francesco", alter="Camera Francy", heatingPage=5, coolingPage=13),
-    Room("Camera_Valentina", alter="Camera Vale", heatingPage=6, coolingPage=14),
+    Room(
+        "Salone",
+        heatingPage=9,
+        coolingPage=12,
+        heatingCode="2200024B2FD101",
+        coolingCode="2200074B2FD101",
+    ),
+    Room(
+        "Studio",
+        heatingPage=7,
+        coolingPage=10,
+        heatingCode="2200004B2FD101",
+        coolingCode="2200054B2FD101",
+    ),
+    Room(
+        "Camera_genitori",
+        alter="Camera genitori",
+        heatingPage=8,
+        coolingPage=11,
+        heatingCode="2200014B2FD101",
+        coolingCode="2200064B2FD101",
+    ),
+    Room(
+        "Camera_Francesco",
+        alter="Camera Francy",
+        heatingPage=5,
+        coolingPage=13,
+        heatingCode="2100004B2FD101",
+        coolingCode="2100044B2FD101",
+    ),
+    Room(
+        "Camera_Valentina",
+        alter="Camera Vale",
+        heatingPage=6,
+        coolingPage=14,
+        heatingCode="2100014B2FD101",
+        coolingCode="2100054B2FD101",
+    ),
 ]
 
 
--- server/functions/exec_on_cmi.py
+++ server/functions/exec_on_cmi.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask
+from functions.constants import main_rooms
 from functions.log import printLog  # type: ignore
 from requests.auth import HTTPBasicAuth
 
@@ -9,6 +10,31 @@ def set_on_off(app: Flask, settings: dict[str, str], on: bool):
 
     requests.get(
         f"http://192.168.0.195/INCLUDE/change.cgi?changeadrx2=0100004F00B100&changetox2={0 if on else 1}",
+        auth=HTTPBasicAuth(settings["username"], settings["password"]),
+        headers={"Referer": "http://192.168.0.195/schema.html"},
+    )
+
+
+def change_temperature(
+    app: Flask,
+    settings: dict[str, str],
+    room_id: int,
+    temperature: float,
+    heating: bool,
+):
+    room = main_rooms[room_id]
+
+    function = "heating" if heating else "cooling"
+    address = room.code(heating)
+
+    printLog(
+        app,
+        "Info",
+        f"Setting target temperature for {function} room '{room.disp_name}' to {temperature}",
+    )
+
+    requests.get(
+        f"http://192.168.0.195/INCLUDE/change.cgi?changeadrx2={address}&changetox2={temperature}",
         auth=HTTPBasicAuth(settings["username"], settings["password"]),
         headers={"Referer": "http://192.168.0.195/schema.html"},
     )
